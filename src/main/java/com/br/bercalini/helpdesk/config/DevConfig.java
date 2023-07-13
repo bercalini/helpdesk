072b914d70974eabf424d081f9fe4f708ca2f633
bercalinidev <alisson.develooper@hotmail.com]>
2023-07-13 10:53:08 -0300

    perfil de desenvolvedor

--- src/main/java/com/br/bercalini/helpdesk/config/DevConfig.java
+++ src/main/java/com/br/bercalini/helpdesk/config/DevConfig.java
@@ -1,22 +1,29 @@
 package com.br.bercalini.helpdesk.config;
 
 import com.br.bercalini.helpdesk.service.DBService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import lombok.extern.slf4j.Slf4j;
+
 @Configuration
-@Profile("test")
+@Profile("dev")
 @Slf4j
-public class TestConfig {
+public class DevConfig {
 
     @Autowired
     private DBService dbService;
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String value;
 
     @Bean
-    public void instanciaDB() {
-        log.info("PERFIL TEST INSTANCIA DO BANCO H2 INICIADO.....");
-        dbService.instanciaDB();
+    public void instanciarDB() {
+        if(value.equals("create")) {
+            log.info("PERFIL DEV INSTANCIA DO BANCO H2 INICIADO.....");
+            dbService.instanciaDB();
+        }
     }
+
 }
